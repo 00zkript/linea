9258a828b87e4b8a1b692f0890546a02d7b4df90
00zkript <60619476+00zkript@users.noreply.github.com>
2023-05-02 17:27:10 -0500

    changes2

--- resources/views/panel/template/index.blade.php
+++ resources/views/panel/template/index.blade.php
@@ -15,8 +15,7 @@
     <link href="{{ asset('generales/font-awesome/6.1.2/css/all.min.css') }}" rel="stylesheet">
     <link href="{{ asset('panel/css/nprogress.css') }}" rel="stylesheet">
     <link href="{{ asset('panel/css/bootstrap-progressbar-3.3.4.min.css') }}" rel="stylesheet">
-    {{-- <link href="{{ asset('panel/css/custom.min.css') }}" rel="stylesheet"> --}}
-    <link href="{{ asset('panel/css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('panel/css/custom.min.css') }}" rel="stylesheet">
     <link href="{{ asset('panel/pnotify/PNotifyBrightTheme.css') }}" rel="stylesheet">
     <link href="{{ asset('panel/fileinput/css/fileinput.min.css') }}" rel="stylesheet">
     <link href="{{ asset('panel/fileinput/themes/explorer-fa/theme.min.css') }}" rel="stylesheet">
@@ -66,7 +65,7 @@
 </head>
 <body class="nav-md">
 
-    {{-- <div class="container body">
+    <div class="container body">
         <div class="main_container">
             <div class="col-md-3 left_col">
                 <div class="left_col scroll-view">
@@ -249,234 +248,9 @@
             </footer>
 
         </div>
-    </div> --}}
+    </div>
 
 
-    <section class="container-fluid">
-
-        <section class="row">
-            <aside class="col-md-2">
-                <section>
-                    <div class="link-home">
-                        <a target="_blank" href="{{ url('/') }}" class="align-content-center d-flex justify-content-center p-2 unlink">
-                            <span><i class="fa fa-shopping-cart"></i></span>
-                            <h2>{{ $empresaGeneral->nombre }}</h2>
-                        </a>
-                    </div>
-
-                    <div class="profile clearfix">
-                        <div class="profile_pic">
-                            @if(empty(auth()->user()->foto))
-                                <img src="{{ asset('panel/default/foto_defecto.jpg') }}" alt="..." class="rounded-circle">
-                            @else
-                                <img src="{{ asset('panel/img/usuarios/'.auth()->user()->foto) }}" alt="..." class="img-circle profile_img">
-                            @endif
-
-                        </div>
-                        <div class="profile_info">
-                            <span>Bienvenido,</span>
-                            <h2>{{ auth()->user()->usuario }}</h2>
-                        </div>
-                        <div class="clearfix"></div>
-                    </div>
-                </section>
-
-                <section>
-                    <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
-                        <div class="menu_section">
-                            <h3>General</h3>
-                            <ul class="nav side-menu ">
-                                <li>
-                                    <a href="{{ route('inicio.index') }}"><i class="fa fa-home"></i> Inicio</a>
-                                </li>
-                                <li>
-                                    <a><i class="fa fa-lock"></i> Seguridad <span class="fa fa-chevron-down"></span></a>
-                                    <ul class="nav child_menu">
-                                        <li>
-                                            <a href="{{ route('usuario.index') }}">Usuarios</a>
-                                        </li>
-
-                                    </ul>
-                                </li>
-
-                                {{-- <li><a><i class="fa fa-shopping-cart"></i> Catálogo <span class="fa fa-chevron-down"></span></a>
-                                    <ul class="nav child_menu">
-                                        <li ><a href="{{ route('marcas.index') }}">Marcas</a></li>
-                                        <li><a href="{{ route('categorias.index') }}">Categorias</a></li>
-                                        <li ><a href="{{ route('section.index') }}">Secciones</a></li>
-                                        <li><a href="{{ route('atributo.index') }}">Atributos</a></li>
-                                        <li><a href="{{ route('productos.index') }}">Productos</a></li>
-                                    </ul>
-                                </li> --}}
-
-                                {{-- <li><a><i class="fa fa-money-check-dollar"></i> Ventas o pedidos <span class="fa fa-chevron-down"></span></a>
-                                    <ul class="nav child_menu">
-                                        <li ><a href="{{ route('cliente.index') }}">Clientes</a></li>
-                                        <li ><a href="{{ route('ventas.index') }}">Ventas</a></li>
-                                        <li><a href="{{ route('cupon.index') }}">Cupones</a></li>
-                                        <li><a href="{{ route('costo-envio.index') }}">Costo de envío</a></li>
-                                    </ul>
-                                </li> --}}
-
-                                {{-- <li><a><i class="fa fa-file"></i> Pagina de Inicio <span class="fa fa-chevron-down"></span></a>
-                                    <ul class="nav child_menu">
-                                        <li ><a href="{{ route('section-home.index') }}">Secciones Home</a></li>
-                                    </ul>
-                                </li> --}}
-
-
-                                <li><a><i class="fa fa-tasks"></i> General <span class="fa fa-chevron-down"></span></a>
-                                    <ul class="nav child_menu">
-                                        <li><a href="{{ route('menu.index') }}">Menú</a></li>
-                                        <li><a href="{{ route('empresa.index') }}">Empresa</a></li>
-                                        <li><a href="{{ route('seo.index') }}">SEO</a></li>
-                                        <li><a href="{{ route('contacto.index') }}">Contacto</a></li>
-                                        <li><a href="{{ route('moneda.index') }}">Moneda</a></li>
-                                        <li><a href="{{ route('terminos-condiciones.index') }}">Términos y condicones</a></li>
-                                        <li><a href="{{ route('politicas-privacidad.index') }}">Políticas de privacidad</a></li>
-                                        {{--<li style="display: none"><a href="{{ route('puntoVentas.index') }}">Puntos de venta</a></li>--}}
-                                    </ul>
-                                </li>
-
-                                {{-- <li><a><i class="fa fa-file-circle-plus"></i> Paginas <span class="fa fa-chevron-down"></span></a>
-                                    <ul class="nav child_menu">
-                                        <li ><a href="{{ route('asesores.index') }}">Asesores</a></li>
-                                        <li><a href="{{ route('pagina.index') }}">Página</a></li>
-                                        <li><a href="{{ route('instagram.index') }}">instagram</a></li>
-                                        <li><a href="{{ route('testimonio.index') }}">Testimonios</a></li>
-                                        <li><a href="{{ route('preguntas-frecuentes.index') }}">Preguntas frecuentes</a></li>
-                                    </ul>
-                                </li> --}}
-
-                                {{-- <li><a><i class="fa fa-image"></i> Banners - Popups<span class="fa fa-chevron-down"></span></a>
-                                    <ul class="nav child_menu">
-                                        <li><a href="{{ route('banner.index') }}">Banners</a></li>
-                                        <li><a href="{{ route('popup.index') }}">Popups</a></li>
-                                    </ul>
-                                </li> --}}
-
-
-                                {{-- <li><a><i class="fa fa-globe"></i> Noticias <span class="fa fa-chevron-down"></span></a>
-                                    <ul class="nav child_menu">
-                                        <li><a href="{{ route('blogCategoria.index') }}">Blog Categoria</a></li>
-                                        <li><a href="{{ route('blog.index') }}">Blog</a></li>
-                                    </ul>
-                                </li> --}}
-
-
-                                {{-- <li><a><i class="fa fa-people-group"></i> Marketing <span class="fa fa-chevron-down"></span></a>
-                                    <ul class="nav child_menu">
-                                        <li><a href="{{ route('suscripcion.index') }}">Lista de suscripciones</a></li>
-                                    </ul>
-                                </li> --}}
-
-                                {{-- <li><a><i class="fa fa-file-text"></i> Reportes <span class="fa fa-chevron-down"></span></a>
-                                    <ul class="nav child_menu">
-                                        <li><a href="{{ route('reporteVentas.index') }}">Ventas</a></li>
-                                    </ul>
-                                </li> --}}
-
-                            </ul>
-                        </div>
-
-                    </div>
-                </section>
-
-                <section>
-                    <div class="sidebar-footer hidden-small">
-
-                        <a class="w-100" data-toggle="tooltip" data-placement="top" title="Cerrar sesión" href="{{ route('panel.login.salir') }}">
-                            <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
-                        </a>
-                    </div>
-                </section>
-            </aside>
-            <main class="col-10 col-md-9">
-                <section>
-                    {{-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                        <a class="navbar-brand" href="#">Navbar</a>
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav mr-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Dropdown
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="#">Action</a>
-                                <a class="dropdown-item" href="#">Another action</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#">Something else here</a>
-                                </div>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link disabled" href="#">Disabled</a>
-                            </li>
-                            </ul>
-                            <form class="form-inline my-2 my-lg-0">
-                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                            </form>
-                        </div>
-                    </nav> --}}
-
-                    <nav class="top_nav">
-                        <div class="nav_menu">
-                            <div class="nav toggle">
-                                <a id="menu_toggle"><i class="fa fa-bars"></i></a>
-                            </div>
-                            <nav class="nav navbar-nav">
-                                <ul class=" navbar-right">
-                                    <li class="nav-item dropdown open" style="padding-left: 15px;">
-                                        <a href="javascript:;" class="user-profile dropdown-toggle" aria-haspopup="true"
-                                        id="navbarDropdown" data-toggle="dropdown" aria-expanded="false">
-                                            @if(empty(auth()->user()->foto))
-                                                <img src="{{ asset('panel/default/foto_defecto.jpg') }}" alt="">{{ auth()->user()->usuario }}
-                                            @else
-                                                <img src="{{ asset('panel/img/usuarios/'.auth()->user()->foto) }}" alt="">{{ auth()->user()->usuario }}
-                                            @endif
-
-                                        </a>
-                                        <div class="dropdown-menu dropdown-usermenu float-right" aria-labelledby="navbarDropdown">
-                                            <a class="dropdown-item" href="{{ route('configuracion.edit') }}">
-                                                <i class="fa fa-cogs float-right"></i> Configuracion
-                                            </a>
-                                            <a class="dropdown-item" href="{{ route('panel.login.salir') }}">
-                                                <i class="fa fa-sign-out float-right"></i> Cerrar sesión
-                                            </a>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </nav>
-                        </div>
-                    </nav>
-                </section>
-                <section class="">
-                    @yield('cuerpo')
-                </section>
-            </main>
-
-        </section>
-
-        <section>
-            <footer>
-                <div class="float-right">
-                    System - CMS Admin by <a target="_blank" href="https://dezain.com.pe/">Dezain Estudio</a>
-                </div>
-                <div class="clearfix"></div>
-            </footer>
-        </section>
-
-    </section>
 
 
 
