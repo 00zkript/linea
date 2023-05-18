f80edb7f38f365a1d470db7dd7b514c499e4ff2e
00zkript <60619476+00zkript@users.noreply.github.com>
2023-05-17 23:28:20 -0500

    changes: modal in caja

--- resources/views/panel/caja/abrirCaja.blade.php
+++ resources/views/panel/caja/abrirCaja.blade.php
@@ -18,14 +18,14 @@
     </div>
 </div>
 
-<div class="modal fade" id="abrirCajaModalCenter" tabindex="-1" role="dialog" aria-labelledby="abrirCajaModalCenterTitle" aria-hidden="true">
+<div class="modal fade" id="abrirCajaModalCenter" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="abrirCajaModalCenterTitle" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="abrirCajaModalCenterTitle">¡Atención!</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                {{-- <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
-                </button>
+                </button> --}}
             </div>
             <div class="modal-body">
                 <form onsubmit="return false;" id="frmAbrirCaja">
